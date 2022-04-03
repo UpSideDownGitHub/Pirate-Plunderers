f74209551ed3c597b6d47dd20978bb7f1ee199e6
LewisMil <102025099+LewisMil@users.noreply.github.com>
2022-04-03 22:47:27 +0100

    UI Work
    Created A Main Menu Scene & Working UI. Also Created A Working Pause Menu. Created The Shop & Upgrade Menu Panels.

---
+++ GroupProjectTest/Assets/Scripts/QuitToDeskTop.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitToDeskTop : MonoBehaviour
+{
+
+
+      public void QuitToDesktop()
+        {
+            Application.Quit();
+        Debug.Log("Quiting to Desktop");
+        }
+}
