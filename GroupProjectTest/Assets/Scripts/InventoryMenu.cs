f3cbb83039eda2e5eff4687b148eea9c42be98dc
LewisMil <102025099+LewisMil@users.noreply.github.com>
2022-04-11 09:56:59 +0100

    Inventory Menu Change
    Made The Inventory Toggle When The "I" Key Is Pressed

---
+++ GroupProjectTest/Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InventoryMenu : MonoBehaviour
+{
+
+
+    public static bool GameIsPaused = false;
+    public string newGameLevel;
+    public GameObject inventoryMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+
+            }
+
+            else
+            {
+                Pause();
+
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        inventoryMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        inventoryMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+
+    }
+
+
+   
+}
+
+
+
+
