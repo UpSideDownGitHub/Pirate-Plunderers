a4bf2a40f461a6363092b4a3a848c390715a4db4
LewisMil <102025099+LewisMil@users.noreply.github.com>
2022-04-11 10:29:03 +0100

    Shop Menu Progress
    Tried Making Shop Menu Work, Doesn't. Created Script & Collision Box Though.

---
+++ GroupProjectTest/Assets/Scripts/ShopIsland.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShopIsland : MonoBehaviour
+{
+
+
+    public static bool GameIsPaused = false;
+    public string newGameLevel;
+    public GameObject shopMenuUI;
+
+
+    // Update is called once per frame
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                if (GameIsPaused)
+                {
+                    Resume();
+
+                }
+
+                else
+                {
+                    Pause();
+
+                }
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        shopMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        shopMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+
+    }
+}
