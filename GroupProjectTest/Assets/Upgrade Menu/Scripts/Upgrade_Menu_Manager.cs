df3fa0c3e8e3aa711c85678397c4d818407d3bc6
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2022-04-21 10:15:25 +0100

    Stats/Description Menu Dynamic
    added functionality to the Stats/Description Menu so when pressing a button it will now display all of the correct information about that weapon

--- GroupProjectTest/Assets/Upgrade Menu/Scripts/Upgrade_Menu_Manager.cs
+++ GroupProjectTest/Assets/Upgrade Menu/Scripts/Upgrade_Menu_Manager.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Upgrade_Menu_Manager : MonoBehaviour
 {
+    [Header("Button Spawning")]
     public GameObject spawnUnder;
     public GameObject prefabButton;
     int currentUpgradeType = 999;
+
+    [Header("Information/Stats Screens")]
+    public GameObject[] cannonScreenUI;
+    public GameObject[] sailScreenUI;
+    public GameObject[] colourScreenUI;
+    public GameObject[] sizeScreenUI;
 
     public void PopulateViewPort(int id, int typeID)
     {
@@ -43,33 +51,91 @@ public class Upgrade_Menu_Manager : MonoBehaviour
         }
     }
 
+    public void TurnOffAllMenus()
+    {
+        cannonScreenUI[0].SetActive(false);
+        sailScreenUI[0].SetActive(false);
+        colourScreenUI[0].SetActive(false);
+        sizeScreenUI[0].SetActive(false);
+    }
+
     public void CannonPressed(int id)
     {
-        // SET THE VARIABLE FOR ALL OF THE INFORMATION IN THE STATS & DESCRIPTION SECTION
+        // TURN OFF MENUS
+        TurnOffAllMenus();
+        cannonScreenUI[0].SetActive(true);
+
+        // LOAD SAVE DATA TO GET INFORMATION OUT
         var SaveData = GenralSaveContainer.Load(Path.Combine(Application.persistentDataPath, "GameSave.xml"));
-        Debug.Log(id);
-        Debug.Log(SaveData.ShipUpgrade.Cannons[id].ID);
+
+        /*
+         *  0 = Main Object
+         *  1 = Damage
+         *  2 = Range
+         *  3 = Description
+         */
+        // SET THE VALUES IN THE MENU TO THE VALUES THAT ARE HELD IN THE SAVE DATA FILE
+        cannonScreenUI[1].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Cannons[id].damage.ToString();
+        cannonScreenUI[2].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Cannons[id].range.ToString();
+        cannonScreenUI[3].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Cannons[id].description.ToString();
     }
     public void SailPressed(int id)
     {
-        // SET THE VARIABLE FOR ALL OF THE INFORMATION IN THE STATS & DESCRIPTION SECTION
+        // TURN OFF MENUS
+        TurnOffAllMenus();
+        sailScreenUI[0].SetActive(true);
+
+        // LOAD SAVE DATA TO GET INFORMATION OUT
         var SaveData = GenralSaveContainer.Load(Path.Combine(Application.persistentDataPath, "GameSave.xml"));
-        Debug.Log(id);
-        Debug.Log(SaveData.ShipUpgrade.Sails[id].ID);
+
+        /*
+         *  0 = Main Object
+         *  1 = Speed
+         *  2 = Turning
+         *  3 = Description
+         */
+        // SET THE VALUES IN THE MENU TO THE VALUES THAT ARE HELD IN THE SAVE DATA FILE
+        sailScreenUI[1].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Sails[id].speed.ToString();
+        sailScreenUI[2].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Sails[id].turning.ToString();
+        sailScreenUI[3].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Sails[id].description.ToString();
     }
     public void ColourPressed(int id)
     {
-        // SET THE VARIABLE FOR ALL OF THE INFORMATION IN THE STATS & DESCRIPTION SECTION
+        // TURN OFF MENUS
+        TurnOffAllMenus();
+        colourScreenUI[0].SetActive(true);
+
+        // LOAD SAVE DATA TO GET INFORMATION OUT
         var SaveData = GenralSaveContainer.Load(Path.Combine(Application.persistentDataPath, "GameSave.xml"));
-        Debug.Log(id);
-        Debug.Log(SaveData.ShipUpgrade.Colours[id].ID);
+
+        /*
+         *  0 = Main Object
+         *  1 = Description
+         */
+        // SET THE VALUES IN THE MENU TO THE VALUES THAT ARE HELD IN THE SAVE DATA FILE
+        colourScreenUI[1].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Colours[id].description.ToString();
     }
     public void SizePressed(int id)
     {
-        // SET THE VARIABLE FOR ALL OF THE INFORMATION IN THE STATS & DESCRIPTION SECTION
+        // TURN OFF MENUS
+        TurnOffAllMenus();
+        sizeScreenUI[0].SetActive(true);
+
+        // LOAD SAVE DATA TO GET INFORMATION OUT
         var SaveData = GenralSaveContainer.Load(Path.Combine(Application.persistentDataPath, "GameSave.xml"));
-        Debug.Log(id);
-        Debug.Log(SaveData.ShipUpgrade.Sizes[id].ID);
+
+        /*
+         *  0 = Main Object
+         *  1 = Health
+         *  2 = Speed
+         *  3 = Turning
+         *  4 = Description
+         */
+        // SET THE VALUES IN THE MENU TO THE VALUES THAT ARE HELD IN THE SAVE DATA FILE
+        sizeScreenUI[1].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Sizes[id].health.ToString();
+        sizeScreenUI[2].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Sizes[id].speed.ToString();
+        sizeScreenUI[3].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Sizes[id].turning.ToString();
+        sizeScreenUI[4].GetComponent<TextMeshProUGUI>().text = SaveData.ShipUpgrade.Sizes[id].description.ToString();
     }
 
 
