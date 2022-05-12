23c7d77cee5c35c44b0633ad009dbeef3599dcfe
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2022-05-12 04:27:39 +0100

    Fixes
    fixed the timings attached to the joysticks and also fixed the death screen not letting you play again when you respawn

--- GroupProjectTest/Assets/Scripts/sceneloader.cs
+++ GroupProjectTest/Assets/Scripts/sceneloader.cs
@@ -6,10 +6,12 @@ public class sceneloader : MonoBehaviour
     // Start is called before the first frame update
     public void backtomennu()
     {
+      Time.timeScale = 1f;
       SceneManager.LoadScene(0);
     }
     public void replay()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(1);
     }
 }
